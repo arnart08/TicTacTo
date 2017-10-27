1c5e74a00560e5f88dd1c7dcdeacabb10a723f83
Arnar Þórðarson <arnart08@ru.is>
2017-10-27 11:35:26 +0000

    so

--- src/selenium/java/is/ru/hugb/TestCalcWeb.java
+++ src/selenium/java/is/ru/hugb/TestCalcWeb.java
@@ -13,44 +13,4 @@ public class TestCalcWeb extends SeleniumTestWrapper {
     driver.get(baseUrl);
     assertEquals("Tic Tac Toe", driver.getTitle());
   }
-
-  @Test
-  public void testSimpleAdd() throws Exception {
-    driver.get(baseUrl);
-    /* Remove Thread.sleep... */
-    Thread.sleep(2000);
-    WebElement input = driver.findElement(By.id("number"));
-    input.sendKeys("1");
-    /* Remove Thread.sleep... */
-    Thread.sleep(2000);
-    input.sendKeys(Keys.RETURN);
-    Thread.sleep(2000);
-    /* ... finish test! */
-  }
-
-@Test
-  public void testSimpleAdds() throws Exception {
-    driver.get(baseUrl);
-    /* Remove Thread.sleep... */
-    Thread.sleep(2000);
-    WebElement input = driver.findElement(By.id("number"));
-    input.sendKeys("a,b,c");
-    /* Remove Thread.sleep... */
-    Thread.sleep(2000);
-    input.sendKeys(Keys.RETURN);
-    Thread.sleep(2000);
-    assertEquals("Error, Illegal string...", driver.findElement(By.id("results")).getText());
-    /* ... finish test! */
-  }
-  
-  @Test
-  public void testString() throws Exception {
-    driver.get(baseUrl);
-    Thread.sleep(2000);
-    WebElement input = driver.findElement(By.id("number"));
-    input.sendKeys("1");
-    input.sendKeys(Keys.RETURN);
-    Thread.sleep(2000);
-    assertEquals("1 = 1", driver.findElement(By.id("results")).getText());
-  }
 }
